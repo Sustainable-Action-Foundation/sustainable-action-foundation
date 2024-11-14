1b00daefdd583acf1e1574f727a3655ec3b7ec87
Axel <axel.schnurer@gmail.com>
2024-11-14 17:44:07 +0100

    Start work on medarbetare page

--- ambient.d.ts
+++ ambient.d.ts
@@ -3,3 +3,19 @@ type Route = {
     href: string, 
     subroutes?: Array<{text: string, href: string}>  
 }
+
+type pbItem = {
+    collectionId: string,
+    collectionName: string, 
+    created: Date,
+    updated: Date,
+    id: string,
+}
+
+type Person = pbItem &  {
+    portrait?: string,
+    name: string,
+    role: string,
+    email?: string,
+    phone?: string,
+}
