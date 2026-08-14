957dd21696699c7c194f040256f980d8b9d92e67
VenaStrom <vena.strom+github@pm.me>
2026-08-14 14:23:50 +0200

    add compat for matrix server both on apex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://sustainable-action-foundation.github.io',
+  site: 'https://sustainable-action.ngo',
   image: {
     remotePatterns: [{
       protocol: "https"
